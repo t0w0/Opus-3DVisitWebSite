54c78798ea1e9b8d9462f964ab003227d879454f
Théo LE GOFF <legoff.theo2@gmail.com>
2016-07-27 17:24:55 +0200

    Impossible to got different

--- js/webGL.js
+++ js/webGL.js
@@ -1,11 +1,23 @@
-var scene, camera, renderer;
+$(function(){
+	var scene, camera, renderer;
     var controls, guiControls, datGUI;
     var stats;
 	var leftPanel;
     var dae, spotLight, lightHelper;
     var SCREEN_WIDTH, SCREEN_HEIGHT;
-	var hoverCubeGeo, hoverCubeMat, hoverCube;
     var loader = new  THREE.ColladaLoader();
+	var domEvents;
+	var interestPoint = [0,0,0, "This Is a Cube", "Lorem Ipsum",
+						 10,30,-20, "This Is another Cube", "Lroem Ipusm",
+						 -10,10,20, "This Is a Cube, again", "Morel Ispum",
+						 40,10,-5, "This Is a ... Cube !", "Romel Sipum",
+						 50,30,-10, "You know what it is", "Losum Iprem",
+						 -5,10,20, "C.U.B.E", "Lorei Upsim",
+						 10,15,20, "Another One", "Orem Lipsum",
+						 -15,1,10, "Interesting... a CUBE !", "Ormel Pumis"
+						]
+	var interestPoints3D = [];
+
     loader.options.convertUpAxis = true;
 
     loader.load('http://localhost/opus0a/models/cath.dae', function (collada){
@@ -33,6 +45,8 @@ var scene, camera, renderer;
         /*add controls*/
         controls = new THREE.OrbitControls( camera, renderer.domElement );
         controls.addEventListener( 'change', render );
+		
+		domEvents = new THREEx.DomEvents(camera, renderer.domElement);
                     
         camera.position.x = 45;
         camera.position.y = 0;
@@ -57,30 +71,8 @@ var scene, camera, renderer;
         scene.add(spotLight);
 		
 		leftPanel = document.getElementById('leftPanel');
-		domEvents = new THREEx.DomEvents(camera, renderer.domElement);
 		
-		/*adds a cube that can be hovered*/
-		var geometry = new THREE.BoxGeometry( .5, .5, .5 );
-		var material = new THREE.MeshBasicMaterial( {color: 0x89A64B} );
-		var cube = new THREE.Mesh( geometry, material );
-		cube.position.x = 10;
-		cube.position.y = -10;
-		cube.position.z = 5;
-		scene.add( cube );
-		
-		domEvents.addEventListener(cube, 'mouseover', function(event) {
-			new_material = new THREE.MeshBasicMaterial({color:0x317DFA});
-			cube.material = new_material;
-			leftPanel.style.display = 'inline';
-			leftPanel.style.opacity = 1;
-			return renderer.render(scene, camera);
-		 });
-		
-      	domEvents.addEventListener(cube, 'mouseout', function(event) {
-			cube.material = material;
-			leftPanel.style.opacity = 0;
-			return renderer.render(scene, camera);
-		});
+		setUpInterestPoints();
 		
         $("#webGL-container").append(renderer.domElement);
 		
@@ -91,6 +83,44 @@ var scene, camera, renderer;
         stats.domElement.style.top = '0px';     
         $("#webGL-container").append( stats.domElement );*/       
     }
+
+	function setUpInterestPoints() {
+		
+		var geometry = new THREE.BoxGeometry( .5, .5, .5 );
+		var material = new THREE.MeshBasicMaterial( {color: 0x89A64B} );
+		var new_material = new THREE.MeshBasicMaterial({color:0x317DFA});
+		interestPoints3D.length = interestPoint.length/5;
+		
+		for (var i = 0 ; i < interestPoint.length ; i += 5){
+			interestPoints3D[i/5] = new THREE.Mesh( geometry, material );
+			interestPoints3D[i/5].position.x = interestPoint[i];
+			interestPoints3D[i/5].position.y = interestPoint[i+1];
+			interestPoints3D[i/5].position.z = interestPoint[i+2];
+		}
+		
+		for (var i = 0 ; i < interestPoints3D.length ; i++) {
+			
+			scene.add(interestPoints3D[i]);
+			
+			domEvents.addEventListener(interestPoints3D[i], 'mouseover', function(event) {
+				interestPoints3D[i].material = new_material;
+				leftPanel.style.display = 'inline';
+				leftPanel.style.opacity = 1;
+				return renderer.render(scene, camera);
+			});
+		
+      		domEvents.addEventListener(interestPoints3D[i], 'mouseout', function(event) {
+				interestPoints3D[i] = material;
+				leftPanel.style.opacity = 0;
+				return renderer.render(scene, camera);
+			});
+		}
+		//document.write(interestPoints3D.join(", "));
+	}
+	
+	function hoverInterestingPoint (event) {
+		
+	}
 
     function render() {   
     
@@ -110,3 +140,4 @@ var scene, camera, renderer;
 
 		renderer.setSize( SCREEN_WIDTH, SCREEN_HEIGHT );
 	});
+});
